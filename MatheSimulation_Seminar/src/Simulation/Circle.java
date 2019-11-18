2216bc3719b4f8cdb6f174a1139bd954c7beb151
sebipf <sebastian.Pfeiffer@hs-furtwangen.de>
2019-11-18 16:07:49 +0100

    Implementing Vektor

--- MatheSimulation_Seminar/src/Simulation/Circle.java
+++ MatheSimulation_Seminar/src/Simulation/Circle.java
@@ -1,45 +1,39 @@
 package Simulation;
 
 public class Circle {
-	double positionX;
-	double positionY;
+	
+	Vektor vektor = new Vektor();
 	double radius;
 	double velocity;
 	double masse;
 	
-	Circle(double _x, double _y, double _r, double _v, double _m){
-		positionX = _x;
-		positionY = _y;
+	Circle(double x, double y, double _r, double _v, double _m){
+		vektor.pX = x;
+		vektor.pY = y;
 		radius = _r;
 		velocity = _v;
 		masse = _m;
 	}
 	
 	public void handleWallCollision() {
-		if (positionX + radius*2 >= _0_Constants.WINDOW_WIDTH || positionX <= 0) {
+		if (vektor.pX + radius*2 >= _0_Constants.WINDOW_WIDTH || vektor.pX <= 0) {
 			velocity *= -1;
 		}
-		if (positionY + radius*2 >= _0_Constants.WINDOW_HEIGHT || positionY <= radius) {
+		if (vektor.pY + radius*2 >= _0_Constants.WINDOW_HEIGHT || vektor.pY <= radius) {
 			velocity *= -1;
 		}
 	}	
 	
 	public boolean hasCollision(Circle circle2){
-		double x1 = this.positionX;
-		double y1 = this.positionY;
-		double x2 = circle2.positionX;
-		double y2 = circle2.positionY;
+		double x1 = vektor.pX;
+		double y1 = vektor.pY;
+		double x2 = circle2.vektor.pX;
+		double y2 = circle2.vektor.pY;
 		double r1 = this.radius;
 		double r2 = circle2.radius;
 		
 		//Distanz zwischen den beiden Mittelpunkten
 		//double distance = java.lang.Math.sqrt(((x1 - x2)*(x1 - x2)) + ((y1 - y2) * (y1 - y2)));
-		System.out.println("Abstand: " + java.lang.Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2) * (y1 - y2)));
-		System.out.println("r1: " + r1);
-		System.out.println("r2: " + r2);
-		System.out.println(x1 + " " + y1);
-		System.out.println(x2 + " " + y2);
-		System.out.println("Radien: " + (r1 + r2));
 		
 		if((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) < (r1 + r2)){
 			return true;
@@ -61,6 +55,52 @@ public class Circle {
 		if(hasCollision(circle2)) {
 			velocity *= -1;
 		}
+		
+		//Rechnung für vollelastischen Stoß
+		Vektor u1 = this.getU(this.vektor, this.getV(this.vektor, circle2.vektor));
+		Vektor u2 = this.getU(circle2.vektor, this.getV(this.vektor, circle2.vektor));
+
+		Vektor b = this.vektor.sub(circle2.vektor);
+		
+		
+		//!!!!!!
+		double k = 20;
+		double l = 20;
+		if(this.vektor.getRelativeAngleBetween(circle2.vektor) == 0 && circle2.velocity == 0)
+		{
+			l = 0;
+		}else if(this.vektor.getRelativeAngleBetween(circle2.vektor) == 0 && this.velocity == 0) {
+			k = 0;
+		}
+		else {
+		    k =  2 * u1.pX * b.pX;
+		    l = -2 * u2.pX * b.pX;
+		}
+		
+		this.vektor.pX = this.vektor.pX - k * b.pX;
+		this.vektor.pY = this.vektor.pY - k * b.pY;
+		
+		circle2.vektor.pX = circle2.vektor.pX - l * b.pX;
+		circle2.vektor.pY = circle2.vektor.pY - l * b.pY;
+		
+		System.out.println("C2X: " + circle2.vektor.pX);
+		System.out.println("C2Y: " + circle2.vektor.pY);
+		System.out.println("C1X: " + this.vektor.pX);
+		System.out.println("C1Y: " + this.vektor.pY);
+	}
+	
+	public Vektor getV(Vektor v1, Vektor v2) {
+		double VX = (v1.pX + v2.pX) / 2;
+		double VY = (v1.pY + v2.pY) / 2;
+		
+		return new Vektor(VX, VY);
+	}
+	
+	public Vektor getU(Vektor v1, Vektor V) {
+		double UX = v1.pX - V.pX;
+		double UY = v1.pY - V.pY;
+		
+		return new Vektor(UX, UY);
 	}
 	
 }
--- MatheSimulation_Seminar/src/Simulation/Simulation2DCollision.java
+++ MatheSimulation_Seminar/src/Simulation/Simulation2DCollision.java
@@ -29,9 +29,9 @@ public class Simulation2DCollision extends JFrame {
 		int m1 = 1;
 		int m2 = 1;
 		int r1 = 30;
-		int r2 = 30;
 		AllCircles[0] = new Circle(150, _0_Constants.WINDOW_HEIGHT / 2, r1, 1, m1);
-		AllCircles[1] = new Circle(_0_Constants.WINDOW_WIDTH / 2, _0_Constants.WINDOW_HEIGHT - r2*2, r2, 1, m2);		
+		int r2 = 30;
+		AllCircles[1] = new Circle(_0_Constants.WINDOW_WIDTH / 2, _0_Constants.WINDOW_HEIGHT - r2*2-5, r2, 1, m2);		
 		
 		testObjekt.setVisible(true);
 		double absT = 0;
@@ -55,7 +55,7 @@ public class Simulation2DCollision extends JFrame {
 		bbg.setColor(Color.LIGHT_GRAY);
 		bbg.fillRect(0, 0, _0_Constants.WINDOW_WIDTH, _0_Constants.WINDOW_HEIGHT);
 		
-		//Abfragen auf Kolision
+		
 		for (Circle circle : AllCircles) {
 			circle.handleWallCollision();
 		}
@@ -63,37 +63,34 @@ public class Simulation2DCollision extends JFrame {
 		AllCircles[0].handleCollision(AllCircles[1]);
 		AllCircles[1].handleCollision(AllCircles[0]);
 	
-		
-		//Zeichnen der Objekte
 		bbg.setColor(Color.RED);
 		
-		//erster circle
-		AllCircles[0].positionX += absT/2 * AllCircles[0].velocity;
-		bbg.fillOval((int)(AllCircles[0].positionX),(int) AllCircles[0].positionY, (int)AllCircles[0].radius * 2, (int)AllCircles[0].radius * 2);
+		//first circle
+		AllCircles[0].vektor.pX += AllCircles[0].velocity;
+		//AllCircles[0].vektor.pY += AllCircles[0].velocity;
+		bbg.fillOval((int)(AllCircles[0].vektor.pX),(int) AllCircles[0].vektor.pY, (int)AllCircles[0].radius * 2, (int)AllCircles[0].radius * 2);
 		
-		//zweiter circle
-		AllCircles[1].positionY -= absT/2 * AllCircles[1].velocity;
-		bbg.fillOval((int)(AllCircles[1].positionX),(int) AllCircles[1].positionY, (int)AllCircles[1].radius * 2, (int)AllCircles[1].radius * 2);
+		//second circle
+		AllCircles[1].vektor.pY -=  AllCircles[1].velocity;
+		bbg.fillOval((int)(AllCircles[1].vektor.pX),(int) AllCircles[1].vektor.pY, (int)AllCircles[1].radius * 2, (int)AllCircles[1].radius * 2);
 
-		//schwerpunkt
+		//Schwerpunkt zeichnen
 		bbg.setColor(Color.GREEN);
-		Point temp = getMainEmphasis(AllCircles[0], AllCircles[1]);
-		bbg.fillOval(temp.x, temp.y, 10, 10);
+		Vektor temp = getMainEmphasis(AllCircles[0], AllCircles[1]);
+		bbg.fillOval((int)temp.pX, (int)temp.pY, 10, 10);
 		
-		//Linie zweischen den beiden Mittelpunkten
 		bbg.setColor(Color.YELLOW);
-		bbg.drawLine((int)(AllCircles[0].positionX + AllCircles[0].radius), (int)(AllCircles[0].positionY + AllCircles[0].radius), (int)(AllCircles[1].positionX + AllCircles[1].radius), (int)(AllCircles[1].positionY + + AllCircles[1].radius));
-
-		//Bild zeichnen/ ausgaben
+		bbg.drawLine((int)(AllCircles[0].vektor.pX + AllCircles[0].radius), (int)(AllCircles[0].vektor.pY + AllCircles[0].radius), (int)(AllCircles[1].vektor.pX + AllCircles[1].radius), (int)(AllCircles[1].vektor.pY + + AllCircles[1].radius));
+		
+		
 		g.drawImage(backBuffer, 0, 0, null);
 
 	}
 	
-	//Koordinaten des Schwerpunktes bestimmen
-	public Point getMainEmphasis(Circle c1, Circle c2) {
-		int pX = (int)(((c1.masse * c1.positionX)+(c2.masse * c2.positionX)) / (c1.masse + c2.masse));
-		int pY = (int)(((c1.masse * c1.positionY)+(c2.masse * c2.positionY)) / (c1.masse + c2.masse));
+	public Vektor getMainEmphasis(Circle c1, Circle c2) {
+		double pX = ((c1.masse * c1.vektor.pX)+(c2.masse * c2.vektor.pX)) / (c1.masse + c2.masse);
+		double pY = ((c1.masse * c1.vektor.pY)+(c2.masse * c2.vektor.pY)) / (c1.masse + c2.masse);
 		
-		return new Point(pX, pY);
+		return new Vektor(pX, pY);
 	}
 }
